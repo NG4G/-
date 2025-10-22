a7c881401a7e2b09c948f5b385a7eb19a9b59b2f
MUKILTEO\1725203 <bnchild01@gmail.com>
2025-10-22 10:24:01 -0700

    Movement, camera controller, worked on attacking

---
+++ Assets/Scripts/topDownMovement.cs
@@ -0,0 +1,50 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class topDownMovement : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 8f;
+
+    private Vector2 movement;
+    private float currentSpeed;
+    private Rigidbody2D rb2D;
+
+    [HideInInspector] public Vector2 direction;
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        currentSpeed = walkSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.linearVelocity = movement * currentSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        movement = ctx.ReadValue<Vector2>();
+
+        if (ctx.ReadValue<Vector2>() != Vector2.zero)
+        {
+            direction = ctx.ReadValue<Vector2>();
+        }
+        
+    }
+
+    public void Run(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1) //Pressed
+        {
+            currentSpeed = runSpeed;
+        }
+        else //Released
+        {
+            currentSpeed = walkSpeed;
+        }
+    }
+}
